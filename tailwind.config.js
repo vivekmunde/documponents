72004c723f88defb0edb016761f3cf791d005ea8
Vivek <vivekmunde@gmail.com>
2023-11-23 12:33:25 +0530

    Primary colour variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
+    colors: {
+      ...colors,
+      primary: {
+        50: '#fdf2f8',
+        100: '#fce7f3',
+        200: '#fbcfe8',
+        300: '#f9a8d4',
+        400: '#f472b6',
+        500: '#ec4899',
+        600: '#db2777',
+        700: '#be185d',
+        800: '#9d174d',
+        900: '#831843',
+        950: '#500724',
+      },
+    },
     extend: {},
   },
   plugins: [],
