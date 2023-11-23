c30f78d303d70ce516e82f87c08fd42cb26d06ad
Vivek <vivekmunde@gmail.com>
2023-11-23 12:35:59 +0530

    Prop types

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { TComponentBaseProps } from './types';
 
-const Root: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className="dark:bg-black dark:text-neutral-300">{children}</div>;
+const Root: React.FC<TComponentBaseProps & React.HTMLProps<HTMLDivElement>> = ({ children, className, ...props }) => {
+  return (
+    <div className={`dark:bg-black dark:text-neutral-300 ${className ?? ''}`} {...props}>
+      {children}
+    </div>
+  );
 };
 
 export default Root;
